87dda5cc6ca7c8578fbaed64b6c017f61f98eb41
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-23 14:31:15 -0300

    Criar entidade cadastro

---
+++ src/main/java/br/com/zup/ZupCadastro/ZupCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupCadastroApplication.class, args);
+	}
+
+}
