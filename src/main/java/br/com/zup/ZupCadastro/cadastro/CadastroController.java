0301b0212f08f2872f6630226cd535bb91b249b7
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-23 14:51:02 -0300

    Criar métodos para salvar cadastro

--- src/main/java/br/com/zup/ZupCadastro/cadastro/CadastroController.java
+++ src/main/java/br/com/zup/ZupCadastro/cadastro/CadastroController.java
@@ -1,13 +1,25 @@
 package br.com.zup.ZupCadastro.cadastro;
 
+import br.com.zup.ZupCadastro.cadastro.dto.CadastroDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cadastros")
 public class CadastroController {
     @Autowired
     private CadastroService cadastroService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void realizarCadastro(@RequestBody CadastroDto cadastroDto){
+        Cadastro cadastro = modelMapper.map(cadastroDto, Cadastro.class);
+
+        cadastroService.salvarCadastro(cadastro);
+    }
 
 }
--- src/main/java/br/com/zup/ZupCadastro/cadastro/CadastroService.java
+++ src/main/java/br/com/zup/ZupCadastro/cadastro/CadastroService.java
@@ -3,9 +3,17 @@ package br.com.zup.ZupCadastro.cadastro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
+
+    public Cadastro salvarCadastro(Cadastro cadastro) {
+        cadastro.setDataDoCadastro(LocalDate.now());
+
+        return cadastroRepository.save(cadastro);
+    }
 
 }
