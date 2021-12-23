3e6c2462ddabb6672d8abc9668185b28750fce0e
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-23 14:35:02 -0300

    Implementar classes da entidade cadastro

---
+++ src/main/java/br/com/zup/ZupCadastro/cadastro/CadastroService.java
@@ -0,0 +1,11 @@
+package br.com.zup.ZupCadastro.cadastro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroService {
+    @Autowired
+    private CadastroRepository cadastroRepository;
+
+}
